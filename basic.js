1c5e6a1d4d0af0597aa6b4a237cceae85fdd3eeb
ndurand1 <npdood@gmail.com>
2021-02-04 17:02:00 -0500

    added launch config

--- basic.js
+++ basic.js
@@ -1,6 +1,8 @@
 const os = require('os');
+const chance = require('chance');
 
+const mychance = new chance();
 
-
-console.log('hi')
-console.log("It looks like you're running a " + os.platform + "machine.")
+console.log('hi');
+console.log("It looks like you're running a " + os.platform + "machine.");
+console.log("Your new fake name is " + mychance.name());
